2ef2acb47f02477d1c3aebaf316dd847561d4cc4
D. Rimron-Soutter <darran@xalior.com>
2019-07-26 19:44:30 +0100

    Clean up old server code

--- server/printers/Pipsta.py
+++ server/printers/Pipsta.py
@@ -1,29 +1,9 @@
 #!/usr/bin/env python
 #
-# PrintShopPrint.py
+# Pipsta.py
 #
-# Original Name - BasicPrint.py
-# Copyright (c) 2014 Able Systems Limited. All rights reserved.
-#
-# Modified by Garry Lancaster 2019 to perform as a simple wifi print server
-# on port 65432 for the ZX Spectrum Next.
-#
-# Modified by D. 'Xalior' Rimron-Soutter 2019 to perform as a slightly more
-# complex wifi print server, also on port 65432, compatible with Garry's
-# version, still for the ZX Spectrum Next.
 from __future__ import print_function
 
-HOST = ''
-PORT = 65432
-
-'''PrintShopPipsta is provided as-is, and is for demonstration
-purposes only. Stale Pixels, or Xalior takes no responsibility
-for any system implementations based on this code.
-
-Copyright (c) 2014 Able Systems Limited. All rights reserved.
-Copyright (c) 2019 Stale Pixels.         Some rights reserved.
-
-'''
 import struct
 
 import usb.core
