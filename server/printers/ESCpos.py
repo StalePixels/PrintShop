a7653cd4fb23e007dabd1d7d0a95b7e07259ce25
D. Rimron-Soutter <darran@xalior.com>
2019-07-26 16:10:50 +0100

    Fix up comments in ESCpos driver

--- server/printers/ESCpos.py
+++ server/printers/ESCpos.py
@@ -1,21 +1,13 @@
+#!/usr/bin/env python
 #
-# PrintShopPrint.py
+# ESCpos.py
 #
-# Original Name - BasicPrint.py
-# Copyright (c) 2014 Able Systems Limited. All rights reserved.
-#
-# Modified by Garry Lancaster 2019 to perform as a simple wifi print server
-# on port 65432 for the ZX Spectrum Next.
-#
-# Modified by D. 'Xalior' Rimron-Soutter 2019 to perform as a slightly more
-# complex wifi print server, also on port 65432, compatible with Garry's
-# version, still for the ZX Spectrum Next.
 from __future__ import print_function
 
 HOST = ''
 PORT = 65432
 
-'''PrintShopPipsta is provided as-is, and is for demonstration
+'''ESCpos driver is provided as-is, and is for demonstration
 purposes only. Stale Pixels, or Xalior takes no responsibility
 for any system implementations based on this code.
 
@@ -60,8 +52,8 @@ class ESCpos(DummyPrinter):
 
 
 
-# Ensure that BasicPrint is ran in a stand-alone fashion (as intended) and not
-# imported as a module. Prevents accidental execution of code.
+# Ensure that ESCpos is never ran in a stand-alone fashion (as intended)
+# and always imported as a module. Prevents accidental execution of code.
 if __name__ == '__main__':
-    main()
+    raise SystemExit
 
